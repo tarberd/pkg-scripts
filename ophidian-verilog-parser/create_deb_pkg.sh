e170d9db49669696425d81e53713b06d5fa16c2b
tarberd <bernardo.mferrari@gmail.com>
2017-12-13 17:04:13 -0200

    Ready to package ophidian-verilog-parser

---
+++ ophidian-verilog-parser/create_deb_pkg.sh
@@ -0,0 +1,25 @@
+PKGNAME="ophidian-verilog-parser"
+PKGVERSION="0.2.31-1"
+
+ROOT=$(pwd)
+SRCROOT=$ROOT/verilog-parser
+PKGROOT=$ROOT/$PKGNAME\_$PKGVERSION
+
+LICENCE_DIR=$PKGROOT/usr/share/doc/ophidian-verilog-parser
+
+mkdir $PKGROOT
+mkdir -p $LICENCE_DIR
+
+cd $SRCROOT
+git submodule update --init
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+make DESTDIR=$PKGROOT install
+cd .. && rm -rf build
+
+cp -r $ROOT/DEBIAN $PKGROOT
+cp $SRCROOT/LICENSE.txt $LICENCE_DIR/copyright
+
+dpkg-deb --build $PKGROOT
+
+rm -rf $PKGROOT
